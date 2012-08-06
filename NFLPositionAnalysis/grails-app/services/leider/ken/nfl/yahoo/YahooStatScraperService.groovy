1b2510af7d185fdf023c8c04e291f94b46d5588e
Kenneth Leider <kennethleider@gmail.com>
2012-08-06 13:59:26 -0600

    Added player scraper

--- NFLPositionAnalysis/grails-app/services/leider/ken/nfl/yahoo/YahooStatScraperService.groovy
+++ NFLPositionAnalysis/grails-app/services/leider/ken/nfl/yahoo/YahooStatScraperService.groovy
@@ -5,17 +5,20 @@ import leider.ken.nfl.stats.Week
 import leider.ken.nfl.stats.Player
 import leider.ken.nfl.stats.PlayerStats
 import leider.ken.nfl.stats.PlayerWeekStats
+import leider.ken.nfl.armchairanalysis.ArmchairPlayerRef
 
 class YahooStatScraperService {
     static transactional = false
     def sessionFactory
     private def slurper = new XmlSlurper(new org.ccil.cowan.tagsoup.Parser())
+    def grailsApplication
     
     def scrape() {
        def seasons = determineSeasons()
+       println seasons
        for (def season in seasons) {
            def weeks = determineWeeks(season)
-//           scrapePlayers(season)
+           scrapePlayers(season)
 //           for (def week in weeks) {
 //               scrapeQBs(week)
 //           }
@@ -25,7 +28,7 @@ class YahooStatScraperService {
     def determineSeasons() {
         process("Seasons",
             "http://sports.yahoo.com/nfl/stats/byposition",
-            { it.parent().@name = 'year' && it.parent().name() == 'select' && it.name() == 'option' && it.text().contains(' Season') },
+            { it.parent().@name == 'year' && it.parent().name() == 'select' && it.name() == 'option' && it.text().contains(' Season') },
             {
                 int year = (it.text() =~  /\d+/)[0].toInteger()
                 return Season.findOrSaveWhere(year : year)
@@ -33,8 +36,63 @@ class YahooStatScraperService {
         )
     }
     
-    def determineWeeks() {
-         
+    def determineWeeks(Season season) {
+        def retval = []
+        for (def number in 1..17) {
+            retval.add(Week.findOrSaveWhere(number : number, season : season))
+        }
+        
+        return retval
+    }
+    
+    def scrapePlayers(Season season) {
+        for (def position in [ 'QB', 'RB', 'WR', 'TE' ]) {
+            process("Players - ${season} - ${position}",
+            "http://sports.yahoo.com/nfl/stats/byposition?pos=${position}&year=season_${season.year}",
+                { it.@class.text().contains("ysprow")},
+                {
+                    def td = it.td
+                    def id = (td[0].a.@href.text()=~ /\d+/)[0].toLong()
+                    def name = td[0].a.text()
+                    
+                    def ref = YahooPlayerRef.get(id)
+                    if (!ref) {
+                        ref = new YahooPlayerRef()
+                        ref.id = id
+                        ref.save()
+                    } 
+                    def player
+                    
+                    def code = grailsApplication.config.yahooLookup[id]
+                    if (code) {
+                        player = ArmchairPlayerRef.findByCode(code)?.player
+                        player.name = name
+                        player.save()
+                    }
+                    
+                    if (!player) {
+                        def matches = Player.findAllByName(name)
+                        if (matches.size() == 1) {
+                            player = matches[0]
+                        } else if (matches.size() > 1) {
+                            matches = matches.findAll { it.position == position }
+                            
+                            if (matches.size() == 1) {
+                                player = matches[0]
+                            }
+                        }
+                    }
+                    
+                    if (player) {
+                        ref.player = player
+                        ref.save()
+                    } else {
+                        println "Unable to find a match for yahoo player: ${i} - ${name}}"
+                    }
+                    
+                }    
+            ) 
+        }
     }
 
     
@@ -43,7 +101,6 @@ class YahooStatScraperService {
         def htmlParser = slurper.parse(input)
         
         def matches = htmlParser.depthFirst().findAll(filter)
-        
         int i = 1
         int records = 100
         long start = System.currentTimeMillis()
